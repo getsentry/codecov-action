180c6bbb9d3e273c5f6ad4a0e1337249f10d788f
mathuraditya724 <mathuraditya724@gmail.com>
2025-12-02 17:43:18 +0530

    init commit

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  input: "src/index.ts",
+  output: {
+    esModule: true,
+    file: "dist/index.js",
+    format: "es",
+  },
+  plugins: [typescript(), nodeResolve({ preferBuiltins: true }), commonjs()],
+};
